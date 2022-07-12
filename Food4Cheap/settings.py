b22b990ab53aa37032ddbd37bfedb7e1f10ca876
Malcolm Zhao <96398778+ZhaoHanrui@users.noreply.github.com>
2022-07-11 21:24:25 -0700

    testing database settings

--- Food4Cheap/settings.py
+++ Food4Cheap/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
